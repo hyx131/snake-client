3d6caf64ad7b3379728cec9a46c7b5aa9cfcdfbf
Selin Huang <selin.hyx@gmail.com>
2019-07-03 18:50:33 +0000

    added movement command with setTimeout function

--- client.js
+++ client.js
@@ -10,10 +10,25 @@ const connect = function() {
   conn.on("data", (data) => {
     console.log("Server says: ", data)
   });
-
+  
   conn.on("connect", () => {
     console.log("Successfully connected to game server!");
     conn.write("Name: 123");
+    setTimeout(() => {
+      conn.write("Move: up");
+    }, 500); 
+    setTimeout(() => {
+      conn.write("Move: left");
+    }, 1000); 
+    setTimeout(() => {
+      conn.write("Move: left");
+    }, 1500); 
+    setTimeout(() => {
+      conn.write("Move: down");
+    }, 2000); 
+    setTimeout(() => {
+      conn.write("Move: down");
+    }, 2500); 
   });
 
   return conn;

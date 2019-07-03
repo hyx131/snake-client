ac41cdcc1c22529257bfc50d19a42df9881bab18
Selin Huang <selin.hyx@gmail.com>
2019-07-03 19:30:21 +0000

    moved setupInput function to a new input.js file

--- play.js
+++ play.js
@@ -6,27 +6,9 @@
 
 
 const { connect } = require('./client');
+const { setupInput } = require("./input");
 
 console.log('Connecting ...');
 connect();
 
-/**
- * Setup User Interface 
- * Specifically, so that we can handle user input via stdin
- */
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-
-  const handleUserInput = function() {
-    stdin.on('data', (key) => {
-      if (key === '\u0003') {
-        process.exit();
-      }
-    });
-  };
-
-  return stdin;
-};
+setupInput();

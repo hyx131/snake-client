73dfdab1ea9820961fd398b64bcf61f33fc7b05b
Selin Huang <selin.hyx@gmail.com>
2019-07-03 19:42:08 +0000

    made changes to the handleUserInput function

--- input.js
+++ input.js
@@ -8,16 +8,15 @@ const setupInput = function() {
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
-
-  const handleUserInput = function() {
-    stdin.on('data', (key) => {
-      if (key === '\u0003') {
-        process.exit();
-      }
-    });
-  }();
+  stdin.on("data", handleUserInput(key));
 
   return stdin;
+};
+
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
 };
 
 module.exports = { setupInput };
